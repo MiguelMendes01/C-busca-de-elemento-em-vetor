817e9d4c0c8859e1c50b99e1d14a39ae90d3fafe
Miguel Mendes <150489109+MiguelMendes01@users.noreply.github.com>
2024-08-08 12:37:56 -0300

    Add files via upload

---
+++ BuscaVetor.c
@@ -0,0 +1,20 @@
+
+int main () {
+    int vet [8], num, achei=0;
+    for (int i=0; i<8; i++){
+        printf("\n Digite o elemento da posição %d: ",i+1);
+        scanf("%d", &vet[i]);
+    }
+    printf("\n\n Qual o número que deseja buscar? ");
+    scanf("%d", &num);
+    for (int i=0; i<8; i++){
+        if(vet[i]==num){
+            printf("\n 0 número %d apareceu na posição %d", num, i+1);
+            achei=1;            
+            }
+            if (achei !=1){
+                printf("\n Elemento não encontrado");
+                system("pause");
+                }
+}
+}
